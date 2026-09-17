cb753ea33314745f6aff2a7e599864e2bc49f10e
idkwtdg <stevenvillafuertebshs@gmail.com>
2026-09-17 23:54:44 +0800

    completed card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,25 +1,28 @@
-
+import "./Card.css"
 
 function Card({ property }) {
     if (!property) return null;
     return (
         <div className="card">
-            {property.superhost? <span className="superhost">Superhost</span> : null}
+            {property.superhost? <span className="superhost">Superhost </span> : null}
             <img src={property.image} alt={property.title} />
-            <h2>{property.title}</h2>
-            <p>{property.description}</p>
-            <div className="capacity">
-                <p className="bedroom"># bedroom</p>
-                <p className="People"># People</p>
+            <div className="card-content">
+                <h2>{property.title}</h2>
+                <p>{property.description}</p>
+                <div className="capacity">
+                    <p className="bedroom"># bedroom</p>
+                    <p className="People"># People</p>
+                </div>
+                <hr/>
+                <div className="price-rate">
+                    <p className="price">${property.price}<span>/Night</span></p>
+                    <span>
+                        <img src="../public/images/starfill.svg" alt="star" />
+                        <p>{property.rating}</p>
+                    </span>
+                </div>
             </div>
-            <hr/>
-            <div className="price-rate">
-                <p className="price">${property.price}<span>/Night</span></p>
-                <span>
-                    <img src="../public/images/starfill.svg" alt="star" />
-                    <p>{property.rating}</p>
-                </span>
-            </div>
+            
         </div>
     )
 }
